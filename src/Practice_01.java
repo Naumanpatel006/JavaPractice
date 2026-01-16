5c94a8c01aa10829abbb266e5d4260ea3032c160
Nauman patel <nypatel006@gmail.com>
2026-01-17 00:25:31 +0100

    add Practice_01.java

---
+++ src/Practice_01.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Practice_01 {
+    public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+        System.out.println("enter any number");
+        int digi = sc.nextInt();
+        System.out.println("your entered number is " + digi);
+
+
+    }
+}
