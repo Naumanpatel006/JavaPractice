0ccdb3e4a8232ef6e1c43e7e831070ff8bb48ede
Nauman patel <nypatel006@gmail.com>
2026-01-16 23:38:05 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JavaPractice
+In this repository i am just working on java and brushing up concepts on object oriented programming.
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main{
+    public static void main(String args[]){
+        //System.out.println("user input");
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println("enter string:");
+        //int a = sc.nextInt();
+        //System.out.println("another number:");
+        //int b = sc.nextInt();
+        //int sum = a+b;
+        //System.out.println("sum:");
+        //System.out.println(sum);
+        //boolean b = sc.hasNextInt();
+        //System.out.println(b);
+        //String str = sc.nextLine();
+        //System.out.println(str);
+        //System.out.println(64==66);
+
+    }
+}
